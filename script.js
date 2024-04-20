f3a9ce2de7eb7dedb44b9e3d4a177e88f69dc59c
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-04-20 20:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function mapLetters(str){
+	let charCount = {};
+
+	for(let i=0;i<str.length;i++) {
+		if(!charCount[str[i]]) {
+			charCount[str[i]] = [i];
+		}
+		else {
+			charCount[str[i]].push(i);
+		}
+	}
+	return charCount;
+}
+
